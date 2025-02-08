b74a17182fd4db16da5b454561f85df9137a84bf
이관영 <hicmcs123@gmail.com>
2025-02-09 02:53:54 +0900

    Container, Center Widget 연습

--- lib/main.dart
+++ lib/main.dart
@@ -4,38 +4,53 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
-        actions: [
-          IconButton(icon: Icon(Icons.home),onPressed: (){
-            print("탭탭");
-          },),
-          Icon(Icons.place)
-        ],
-        centerTitle: false,
-        title: Text('this is app bar'),
+        title: Text('Study to Container'),
       ),
-      body: TestWidget(),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(
-          Icons.bug_report
-        ),
-        onPressed: (){
-          print("탭버그!");
-        },
-      ),
+      body: CustomContainer(),
     ),
   ));
 }
 
-class TestWidget extends StatelessWidget {
-  const TestWidget({super.key});
+class CustomContainer extends StatelessWidget {
+  const CustomContainer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
+    return Container(
+      width: 100,
+      height: 100,
+      padding: EdgeInsets.fromLTRB(10, 12, 10, 12),
+      decoration: BoxDecoration(
+          // boxDecoration 사용시에는 container 밖에 color 옵션 쓰면 에러발생
+          color: Colors.amber,
+          border: Border.all(
+            color: Colors.red,
+            width: 5,
+            style: BorderStyle.solid,
+          ),
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color:Colors.grey.withOpacity(0.3),
+              offset: Offset(5, 1),
+              blurRadius: 10,
+              spreadRadius: 10
+            ),
+            BoxShadow(
+                color:Colors.pink.withOpacity(0.3),
+                offset: Offset(-10, -10),
+                blurRadius: 10,
+                spreadRadius: 10
+            ),
+          ]),
+      margin: EdgeInsets.symmetric(
+        vertical: 12,
+        horizontal: 10,
+      ),
       child: Center(
-        child: Text(
-          'hello flutter',
-          style: TextStyle(color: Colors.black, fontSize: 60),
+        child: Container(
+          color: Colors.blueAccent,
+          child: Text('하이요'),
         ),
       ),
     );
